52e6c7198edf935d4406885e6f0827050a1b7f53
eder.dionisio <eder.dionisio@dextra-sw.com>
2021-10-01 14:09:11 -0300

    Criando rotinas.

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `empresa`(
+    `id` BIGINT(255) NOT NULL,
+    `cnpj` VARCHAR(255) NOT NULL,
+    `data_atualizacao` DATETIME NOT NULL,
+    `data_criacao` DATETIME NOT NULL,
+    `razao_social` VARCHAR(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
