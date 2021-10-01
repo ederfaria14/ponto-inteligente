cecf0a5cf176906ab4b9166429569eb8f87ccd0f
eder.dionisio <eder.dionisio@dextra-sw.com>
2021-10-01 10:19:02 -0300

    Teste

---
+++ PontoInteligente/src/main/java/com/ederfaria/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,11 @@
+package com.ederfaria.pontointeligente.api.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa {
+
+}
